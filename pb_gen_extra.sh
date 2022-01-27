8d0f087d862760e2aadf88bb23e4475ee7825afc
starudream <justwangsheng@gmail.com>
2022-01-27 17:43:47 +0800

    fix: fix ci

--- pb_gen_extra.sh
+++ pb_gen_extra.sh
@@ -6,7 +6,7 @@ VERSION=$(git describe --exact-match --tags HEAD 2>/dev/null || echo "latest")
 GITHASH=$(git rev-parse --short HEAD)
 BUILT=$(date +%FT%T%z)
 
-cp -r dist/ pb/
+cp -r dist/* pb/
 
 pushd pb >/dev/null
 
